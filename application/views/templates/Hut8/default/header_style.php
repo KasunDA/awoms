c771c5d4fbe93db711df4b5fdc692053178b9bfc
dirt <flacodirt@awoms.com>
2015-03-22 22:00:51 -0400

    GP Header CSS

--- application/views/templates/Hut8/default/header_style.php
+++ application/views/templates/Hut8/default/header_style.php
@@ -1,17 +1,15 @@
-        <!-------------------------------->
-        <!-- Brand Style -->
-        <!-------------------------------->
-        <link rel="stylesheet" href="/css/Hut8/Hut8.css">
 
-        <!-- Wow slider -->
-        <link rel="stylesheet" href="/css/Hut8/old/wowslider/style.css"/>
+<!-- Custom Loaded CSS (Slider + Fonts) for Hut -->
 
-        <!-- FONT: Miso -->
-        <link rel="stylesheet" href="/css/Hut8/old/fonts/miso/stylesheet.css"/>
+<!-- Wow slider -->
+<link rel="stylesheet" href="/css/Hut8/old/wowslider/style.css"/>
 
-        <!-- FONT: Comfortaa -->
-        <link rel="stylesheet" href="/css/Hut8/old/fonts/Comfortaa/stylesheet.css"/>
+<!-- FONT: Miso -->
+<link rel="stylesheet" href="/css/Hut8/old/fonts/miso/stylesheet.css"/>
 
-        <!-- FONT: Eurostile -->
-        <link rel="stylesheet" href="/css/Hut8/old/fonts/eurostile/stylesheet.css"/>
-        <!-------------------------------->
+<!-- FONT: Comfortaa -->
+<link rel="stylesheet" href="/css/Hut8/old/fonts/Comfortaa/stylesheet.css"/>
+
+<!-- FONT: Eurostile -->
+<link rel="stylesheet" href="/css/Hut8/old/fonts/eurostile/stylesheet.css"/>
+<!-------------------------------->
